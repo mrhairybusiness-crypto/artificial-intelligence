9b6f6b924e9c5ec656d668917af0f9512267ded7
mrhairybusiness-crypto <mrhairybusiness@gmail.com>
2026-04-15 14:48:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ setTimeout(() => {
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
                     model: "llama-3.1-8b-instant",
-                    messages: [{ role: "user", content: userInput + "|" + content + "|" + "if you give the user code, make sure it is inside the text Code: [type the code here.] :Code"}]
+                    messages: [{ role: "user", content: userInput + "|" + content + "|" + "MAKE SURE EVERY SNIPPET OF CODE YOU ADD SHOULD BE INSIDE {}!"}]
                 })
             });
 
@@ -46,7 +46,7 @@ setTimeout(() => {
                 const aiResult = data.choices[0].message.content;
 
                 // 1. Remove the quotes
-                var regex = /Code: (.+?) :Code/g; 
+                var regex = /{(.+?)}/g; 
                 
                 // 2. Spread the iterator into an array to use .map()
                 var findings = [...aiResult.matchAll(regex)]; 
